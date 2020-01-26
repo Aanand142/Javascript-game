4768624f7b5cd52ccc7ea1858dae0401b120166b
Aanand Thakur <59300930+Aanand142@users.noreply.github.com>
2020-01-26 12:25:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+
+var userscore=0;
+var computerscore=0;
+var userscore_span= document.getElementById("user-score");
+var computerscore_span= document.getElementById("computer-score");
+const scoreboard_div= document.querySelector(".score-board");
+const result_div= document.querySelector(".result");
+const rock_div= document.getElementById("r");
+const paper_div= document.getElementById("p");
+const scissors_div= document.getElementById("s");
+
+
+function convert(letter)
+{
+    if(letter==="r") return "Rock";
+    if (letter==="p") return "Paper";
+    return "scissors";
+
+}
+
+function win(user, computer){
+      
+    userscore++;
+    userscore_span.innerHTML=userscore;
+    const small_user="user".fontsize(3).sup ;
+    const small_computer="computer".fontsize(3).sup ;
+    result_div.innerHTML=convert(user)+" beats "+convert(computer);  
+    
+
+}
+
+
+function lose(user, computer){
+    
+    computerscore++;
+    computerscore_span.innerHTML=computerscore;
+    result_div.innerHTML=convert(computer)+" beats "+ convert(user);
+        
+}
+    
+
+function draw(){
+
+    result_div.innerHTML="ITS A DRAW PHEWW!!";
+    }
+
+function getcomputerchoice(){
+    const choices= ['r', 'p', 's'];
+    const randomnumber= Math.floor(Math.random()*3);
+    return choices[randomnumber];
+
+
+}
+function game(userchoice){
+    const computerchoice= getcomputerchoice();
+    switch(userchoice + computerchoice)
+    {
+        case "rs":
+        case "pr":
+        case "sp":
+        win(userchoice, computerchoice);
+
+        break;
+
+        case "rp":
+        case "sr":
+        case "ps":
+        lose(userchoice, computerchoice);
+        break;
+
+        case "ss":
+        case "rr":
+        case "pp":
+        draw();
+        break;
+
+
+
+    }
+}
+
+
+
+
+
+rock_div.addEventListener('click', function(){
+
+ game("r");
+ })
+
+
+paper_div.addEventListener('click', function(){
+    
+    game("p");
+        
+    })
+
+    
+scissors_div.addEventListener('click', function(){
+    
+    game("s");
+        
+    })
